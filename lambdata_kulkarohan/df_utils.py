c7d8cb6ac4f2cd7d51734665389404c347fb4260
“kulkarohan” <kulkarohan@users.noreply.github.com>
2019-09-30 16:05:12 -0400

    added 'isna' and 'confusion_matrix' helper functions

--- lambdata_kulkarohan/df_utils.py
+++ lambdata_kulkarohan/df_utils.py
@@ -3,5 +3,9 @@ Utility functions for working with DataFrames
 """
 
 import pandas as pd 
+from sklearn.metrics import confusion_matrix
 
 TEST_DF = pd.DataFrame([1, 2, 3])
+
+isna = pd.isna()
+confusion_matrix = confusion_matrix()
