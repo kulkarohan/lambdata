a74efdf18fa2b28b4bd83e54b78ee36e2d635b45
“kulkarohan” <kulkarohan@users.noreply.github.com>
2019-10-01 17:38:42 -0400

    updated functions and added docstrings

--- lambdata_kulkarohan/df_utils.py
+++ lambdata_kulkarohan/df_utils.py
@@ -1,21 +1,46 @@
 """
 Utility functions for working with DataFrames
+
+Sample Data
+-----------
+- DF_ISNA
+- DF_SPLIT
 """
-
+# Imports
 import pandas as pd 
+import numpy as np 
 
-TEST_DF = pd.DataFrame([1, 2, 3])
 
-# Check DataFrame for nulls
-def isNA(a):
-    return a.isna().sum(ascending=False)
+# Sample DataFrame for isNA()
+DF_ISNA = pd.DataFrame({
+    "num_cars":[4, 1, 7, 2, np.nan],
+    "num_houses":[1, np.nan, 3, 2, 2]
+})
 
-# Split date into multiple columns
+# Sample DataFrame for split_date()
+DF_SPLIT = pd.DataFrame({'Date':['11/8/2011', '04/23/2008', '10/2/2019']})
+
+
+def isNA(df):
+    """ 
+    Input a DataFrame and isNA(df) will return the number of NaN values.
+
+    Use the already created DataFrame DF_ISNA to test.
+    """
+    print(df.isna().sum())
+    return df.isna().sum()
+
 def split_date(df):
-    df = df.copy()
+    """ 
+    Input a DataFrame with a single column of datetime values and label the
+    column 'Date'. Calling split_date(df) will split the datetime values each 
+    into their own column and add them to the DataFrame. 
 
-    df['Year'] = df.dt.year
-    df['Month'] = df.dt.month
-    df['Day'] = df.dt.day
+    Use the already created DataFrame DF_SPLIT to test.
+    """
+    df['Year'] = pd.to_datetime(df['Date']).dt.year
+    df['Month'] = pd.to_datetime(df['Date']).dt.month
+    df['Day'] = pd.to_datetime(df['Date']).dt.day
 
+    print(df)
     return df
